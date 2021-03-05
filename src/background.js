5e53047484e9b8cabe3e9680f9670a54bd3e26cc
Matěj Novotný <485457@muni.cz>
2021-03-06 00:17:53 +0100

    fix: size

--- src/background.js
+++ src/background.js
@@ -16,9 +16,9 @@ async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 1200,
-    height: 650,
+    height: 660,
     minWidth: 1200,
-    minHeight: 650,
+    minHeight: 660,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
